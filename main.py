3bf3a8b961fa34f3eedfab740f59befd7a10f7de
vishal <vivevich3@gmail.com>
2022-12-05 20:02:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+def tuple1(a):
+    tuple=(10,20,30,40,50)
+    len(tuple)
+def tuple2(a):
+    tuple=tuple1()
+    print (tuple)
+def tuple3(a):
+    tuple=(10,20,30,40,50)
+    tuple.count(10)
+    print(tuple)
+def tuple4(a):
+    tuple=(10,20,30,40,50)
+    tuple.index(30)
+def tuple5(a):
+    tuple=(10,20,30,40,50)
+    sorted(tuple)
+def tuple6(a):
+    tuple=(10,20,30,40,50)
+    min(tuple)
+def tuple7(a):
+    tuple=(10,20,30,40,50)
+    max(tuple)
+def tuple8(a):
+    tuple=(10,20,30,40,50)
+    sum(tuple)
+#dictionary
+def dictionary1(a):
+    pair=[("mohan",95),("sangeetha",89),("suhel",55),("ram",89)]
+    dict1=dict(pair)
+def dictionary2(a):
+    dict= [("mohan", 95), ("sangeetha", 89), ("suhel", 55), ("ram", 89)]
+    dict.keys()
+def dictionary3(a):
+    dict= [("mohan", 95), ("sangeetha", 89), ("suhel", 55), ("ram", 89)]
+    dict.values
+    
+
+
+
+
+
